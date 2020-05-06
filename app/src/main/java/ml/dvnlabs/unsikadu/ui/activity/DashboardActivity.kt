afc313aa97b325d42c9955a18e06627273a468d4
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2020-05-06 14:11:06 +0700

    Fragment Setting initialization

--- app/src/main/java/ml/dvnlabs/unsikadu/ui/activity/DashboardActivity.kt
+++ app/src/main/java/ml/dvnlabs/unsikadu/ui/activity/DashboardActivity.kt
@@ -12,6 +12,7 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -20,6 +21,13 @@ import android.widget.RelativeLayout
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.wang.avi.AVLoadingIndicatorView
 import kotlinx.coroutines.Dispatchers
--- app/src/main/java/ml/dvnlabs/unsikadu/ui/fragment/DashboardSetting.kt
+++ app/src/main/java/ml/dvnlabs/unsikadu/ui/fragment/DashboardSetting.kt
@@ -7,16 +7,70 @@
 
 package ml.dvnlabs.unsikadu.ui.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.frame_setting.*
 
 import ml.dvnlabs.unsikadu.R
+import ml.dvnlabs.unsikadu.model.StudentInfo
+import ml.dvnlabs.unsikadu.viewmodel.StudentsViewModel
+
 class DashboardSetting : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view : View = inflater.inflate(R.layout.frame_setting,container,false)
+    private lateinit var viewModel: StudentsViewModel
+    private var studentInfo: StudentInfo? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.frame_setting, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = activity?.run {
+            ViewModelProvider(this)[StudentsViewModel::class.java]
+        } ?: throw Exception("Invalid")
+
+        studentInfo = viewModel.students
+
+        if (studentInfo != null) {
+            refreshData()
+        }
+        settingLogoutProfile.setOnClickListener {
+            Toast.makeText(requireContext(), "Not Implemented", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun refreshData() {
+        Glide.with(this).applyDefaultRequestOptions(
+            RequestOptions()
+                .placeholder(R.drawable.ic_mortarboard)
+                .error(R.drawable.ic_mortarboard)
+        )
+            .load(studentInfo!!.pictureUrl)
+            .transition(DrawableTransitionOptions().crossFade())
+            .apply(
+                RequestOptions.bitmapTransform(CircleCrop())
+                    .diskCacheStrategy(DiskCacheStrategy.ALL).override(300, 600)
+            )
+            .into(settingProfileIMG)
+
+        settingName.text = studentInfo!!.name
+        settingFaculty.text = studentInfo!!.faculty
+        settingMajor.text = studentInfo!!.major
+
     }
 }
